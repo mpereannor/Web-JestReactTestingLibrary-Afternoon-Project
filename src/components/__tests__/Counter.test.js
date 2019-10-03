5ae965d7d31fc35db3ce05cb83e89b9eebd233f7
chudnofskyATD <mpereannor@gmail.com>
2019-10-03 14:53:36 +0000

    implemented decrement and reset, counter.test.js

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -39,10 +39,25 @@ describe('Counter component', () => {
 
   it('can decrement the count by one by clicking decrement', () => {
     // implement
+
+    const decButton = tools.queryByTestId('decButton');
+
+    rtl.fireEvent.click(decButton);
+    expect(tools.queryByText(/2/)).not.toBeInTheDocument();
+    expect(tools.queryByText(/1/)).toBeInTheDocument();
+
+    rtl.fireEvent.click(decButton);
+    expect(tools.queryByText(/1/)).not.toBeInTheDocument();
+  
   });
 
   it('can reset the count clicking rest', () => {
     // implement
+
+    const resetButton = tools.queryByTestId('resetButton');
+
+    rtl.fireEvent.click(resetButton);
+    expect(tools.queryByText(/0/)).toBeInTheDocument();
   });
 
   it('prevents the count from going over an upper limit', () => {
